b5ef5e7b6aab026c1770fc54f4f1922b4b48bf34
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-02-21 11:17:45 +0300

    added display like function

---
+++ src/modules/displayLikes.js
@@ -0,0 +1,19 @@
+export const displayLikes = async (likes) => {
+  likes.forEach((like) => {
+    const elementid = `show-${like.item_id}`;
+    const displayLike = document.getElementById(elementid);
+    if (displayLike !== null) {
+      const likeNumbers = displayLike.querySelector('.like-numbers');
+      likeNumbers.innerHTML = `${like.likes}`;
+    }
+  });
+};
+
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rdCMW54T0sQtFphOfxh4/likes/';
+
+export const getLikes = async () => {
+  let likes = [];
+  const response = await fetch(url);
+  likes = await response.json();
+  displayLikes(likes);
+};

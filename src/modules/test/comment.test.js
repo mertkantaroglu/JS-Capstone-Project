4ec205c6c1c078287788c21190e6794c62eb8b35
unknown <sohailshahidi1435@gmail.com>
2023-02-23 17:00:44 +0430

    Add test for the comments

---
+++ src/modules/test/comment.test.js
@@ -0,0 +1,20 @@
+import { counter } from '../comments.js';
+
+describe('counter', () => {
+  test('returns the number of comments', () => {
+    const comments = [      { creation_date: '2023-02-23', username: 'Sohail', comment: 'Perfect' },
+                            { creation_date: '2023-02-23', username: 'Mert', comment: 'Great Show' },
+                            { creation_date: '2023-02-24', username: 'Ahmed', comment: 'The best show ever' },
+                      ];
+    const expectedCount = 3;
+    const actualCount = counter(comments);
+    expect(actualCount).toEqual(expectedCount);
+  });
+
+  test('returns 0 when there is an error', () => {
+    const error = { error: 'An error occurred' };
+    const expectedCount = 0;
+    const actualCount = counter(error);
+    expect(actualCount).toEqual(expectedCount);
+  });
+});

071df23ba4e7d081f68d5a7044176682f2f0523d
unknown <sohailshahidi1435@gmail.com>
2023-02-23 13:18:27 +0430

    Update Like files

--- src/modules/addLikes.js
+++ src/modules/addLikes.js
@@ -1,16 +1,8 @@
-import { displayLikes } from './displayLikes.js';
+import { getLikes } from './displayLikes.js';
 
-// Get Likes
 const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rdCMW54T0sQtFphOfxh4/likes/';
 
-export const getLikes = async () => {
-  let likes = [];
-  const response = await fetch(url);
-  likes = await response.json();
-  displayLikes(likes);
-};
-
-export const addLike = async (id) => {
+const addLike = async (id) => {
   await fetch(url, {
     method: 'POST',
     body: JSON.stringify({
@@ -22,3 +14,5 @@ export const addLike = async (id) => {
   });
   getLikes();
 };
+
+export default addLike;
--- src/modules/displayLikes.js
+++ src/modules/displayLikes.js
@@ -1,31 +1,18 @@
-export const displayLikes = async (likes) => {
-  likes.forEach((like) => {
-    const elementid = `show-${like.item_id}`;
-    const displayLike = document.getElementById(elementid);
-    if (displayLike !== null) {
+export async function displayLikes(likes) {
+  for (const like of likes) {
+    const elementId = `show-${like.item_id}`;
+    const displayLike = document.getElementById(elementId);
+    if (displayLike) {
       const likeNumbers = displayLike.querySelector('.like-numbers');
-      likeNumbers.innerHTML = `${like.likes}`;
+      likeNumbers.textContent = like.likes.toString();
     }
-  });
-};
+  }
+}
 
 export const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rdCMW54T0sQtFphOfxh4/likes/';
-const getLikes = async () => {
+export const getLikes = async () => {
   let likes = [];
   const response = await fetch(url);
   likes = await response.json();
   displayLikes(likes);
-};
-
-export const addLike = async (id) => {
-  await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      item_id: id,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  getLikes();
 };
